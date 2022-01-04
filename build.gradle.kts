45d60de131cd7539d2d1a95976c056360ad42480
Alexandre Gressier <alexandre@gressier.dev>
2022-01-04 14:06:59 +0100

    Define version properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,15 @@
-// Versions ----------------------------------------------------------------------------------------
+// Properties --------------------------------------------------------------------------------------
+
+val versionFooBarAndroid by extra("0.1.0")
+val versionCodeFooBarAndroid by extra(1)
+val versionFooBarIos by extra("0.1.0")
+val versionFooBarShared by extra("0.1.0")
 
 val versionGradle by extra("7.3.3")
+val versionJava by extra(JavaVersion.VERSION_11)
+val versionAndroidApi by extra(32)
+val versionMinAndroidApi by extra(26)
+val versionMinIos by extra("14.1")
 
 // Plugins -----------------------------------------------------------------------------------------
 
